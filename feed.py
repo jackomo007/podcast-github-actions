03432da545c5cf190428aaa5ae42e140ea56610f
Angel <jeal.code47@gmail.com>
2024-10-20 10:50:42 -0300

    fix: rss type file

--- feed.py
+++ feed.py
@@ -5,8 +5,8 @@ with open('feed.yaml', 'r') as file:
     yaml_data = yaml.safe_load(file)
 
     rss_element = xlm_tree.Element('rss', {'version':'2.0',
-    'xmls:itunes': 'http://www.itunes.com/dtds/podcast-1.0.dtd',
-    'xmls:content': 'http://purl.org/rss/1.0/modules/content/'})
+    'xmlns:itunes': 'http://www.itunes.com/dtds/podcast-1.0.dtd',
+    'xmlns:content': 'http://purl.org/rss/1.0/modules/content/'})
 
 channel_element = xlm_tree.SubElement(rss_element, 'channel')
 link_prefix = yaml_data['link']
